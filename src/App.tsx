2e687d0b337a8abee4874222033767609bf586e0
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-04-18 10:36:21 +0300

    Добавила роутинги и react.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import Counter from "./components/Counter";
-import './index.scss';
+import "./index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { Suspense } from "react";
 
 const App = () => {
-    return (
-        <div className="app">
-            asdasdASFASFa
-            <Counter />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
