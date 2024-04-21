823f056aee303131ad1a6927f4768bc3460bd4dd
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-04-21 15:32:04 +0300

    Начала работу с методологией Feature-Sliced

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import "./styles/index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { useTheme } from "./providers/ThemProvider/lib/useTheme";
+import { classNames } from "../shared/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <button onClick={toggleTheme}>Them</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
