77777846c279cd7c0e21cef39ecd5a5203d99f45
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-05-10 11:47:37 +0300

    Сделала анимацию открытия-закрытия для модального окна

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,29 +1,39 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import style from "./Modal.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
 interface ModalProps {
   children?: ReactNode;
   isOpend: boolean;
   onClose?: () => void;
 }
 const Modal = ({ children, isOpend = false, onClose }: ModalProps) => {
+  const [closing, setClosing] = useState(false);
+
   const OnContentClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
   const CloseHandler = () => {
     if (onClose) {
-      onClose();
+      setClosing(true);
+      setTimeout(() => {
+        onClose();
+        setClosing(false);
+      }, 200);
     }
   };
   return (
-    isOpend && (
-      <div className={style.Modal}>
-        <div className={style.overlay} onClick={CloseHandler}>
-          <div className={style.content} onClick={OnContentClick}>
-            {children}
-          </div>
+    <div
+      className={classNames(style.Modal, {
+        [style.opened]: isOpend,
+        [style.closing]: closing,
+      })}
+    >
+      <div className={style.overlay} onClick={CloseHandler}>
+        <div className={style.content} onClick={OnContentClick}>
+          {children}
         </div>
       </div>
-    )
+    </div>
   );
 };
 
